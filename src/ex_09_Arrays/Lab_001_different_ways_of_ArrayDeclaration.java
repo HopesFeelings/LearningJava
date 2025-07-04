c6e546bb0ce55d84f231cfc00be1282f30069469
Gaurav Sonawane <gsonawane505@gmail.com>
2025-07-04 15:16:19 +0530

    4 Jully Second

--- src/ex_09_Arrays/Lab_001_different_ways_of_ArrayDeclaration.java
+++ src/ex_09_Arrays/Lab_001_different_ways_of_ArrayDeclaration.java
@@ -16,5 +16,41 @@ public class Lab_001_different_ways_of_ArrayDeclaration {
         System.out.println(System.identityHashCode(arr[0]));
 
         System.out.println(System.identityHashCode(arr[1]));
+
+
+        // Second Way
+
+        int[] arr2 = new int[4];
+
+        arr2[0] = 20;
+        arr2[1] = 50;
+
+        System.out.println(arr2[1]);
+        System.out.println(arr2.length);
+
+
+        // Third Way
+
+        int[] arr3 = new int[] {
+            1,2                                // size automatically inferred
+        };
+
+        System.out.println(arr3.length);
+
+
+
+        int[][] twod = {
+                {1,2}
+        };
+
+        int [][] twod2 = new int[][]{
+                {2,3},
+                {5,6},
+                {4,6},
+                {7,8}
+        };
+
+        System.out.println(twod2.length);
+
     }
 }
